562681dba3b4fc2aff4b231c17ed2abfc56f41d6
MaksymAfanasiev <afanasiev.maksym@pdffiller.team>
2020-12-30 16:11:42 +0200

    fixed bugs

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,4 +2,5 @@
 import printName from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(`Hello ${printName('May I have your name? ')}`);
+const userName = printName('May I have your name? ');
+console.log(`Hello ${userName}`);
